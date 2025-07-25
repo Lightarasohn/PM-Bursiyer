e3ab5374828e7be36762468717a8131b0c11248b
tugbademir <tugbademir@pro-minds.com>
2025-07-25 14:31:01 +0300

    25.07

---
+++ API/Interfaces/IScholarAddService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs.ScholarAddDTOs;
+
+namespace API.Interfaces
+{
+    public interface IScholarAddService
+    {
+        public Task<bool> AddScholar(ScholarAddDto scholarAddDto);
+    }
+}
---
+++ API/Services/ScholarAddService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.DTOs.ScholarAddDTOs;
+using API.Interfaces;
+
+namespace API.Services
+{
+    public class ScholarAddService : IScholarAddService
+    {
+        public Task<bool> AddScholar(ScholarAddDto scholarAddDto)
+        {
+            
+            
+            throw new NotImplementedException();
+        }
+    }
+}
