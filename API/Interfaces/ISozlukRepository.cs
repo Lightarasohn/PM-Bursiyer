f4f30f7df0e714951e9da107029bb4cd8d2cfbf2
tugbademir <tugbademir@pro-minds.com>
2025-07-28 10:23:19 +0300

    SozlukDto

---
+++ API/Interfaces/ISozlukRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs.SozlukDTOs;
+using API.Models;
+
+namespace API.Interfaces
+{
+    public interface ISozlukRepository
+    {
+        Task<IEnumerable<Sozluk>> GetAllDictionaryValues();
+        Task<Sozluk> AddValueAsync(SozlukDTO sozlukDTO);
+        Task<Sozluk> UpdateValueAsync(SozlukDTO sozlukDTO, int id);
+        Task<Sozluk> DeleteValueAsync(int id);
+    }
+}
