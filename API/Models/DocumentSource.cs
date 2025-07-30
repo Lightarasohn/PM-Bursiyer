5c0a481dc5689a72e9cfb4cf834b5242fbc5c164
Lightarasohn <onur.demir.developer@outlook.com>
2025-07-30 14:38:35 +0300

    Document upload db pull

---
+++ API/Models/DocumentSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models;
+
+public partial class DocumentSource
+{
+    public int Id { get; set; }
+
+    public string? SourceName { get; set; }
+
+    public int? CreUser { get; set; }
+
+    public DateOnly? CreDate { get; set; }
+
+    public int? UpdUser { get; set; }
+
+    public DateOnly? UpdDate { get; set; }
+
+    public int? DelUser { get; set; }
+
+    public DateOnly? DelDate { get; set; }
+
+    public bool? Deleted { get; set; }
+
+    public virtual ICollection<Document> Documents { get; set; } = new List<Document>();
+}
---
+++ API/Models/ScholarDocument.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models;
+
+public partial class ScholarDocument
+{
+    public int Id { get; set; }
+
+    public int? ScholarId { get; set; }
+
+    public int DocumentId { get; set; }
+
+    public int? CreUser { get; set; }
+
+    public DateOnly? CreDate { get; set; }
+
+    public int? UpdUser { get; set; }
+
+    public DateOnly? UpdDate { get; set; }
+
+    public int? DelUser { get; set; }
+
+    public DateOnly? DelDate { get; set; }
+
+    public bool? Deleted { get; set; }
+
+    public virtual Document Document { get; set; } = null!;
+
+    public virtual Scholar? Scholar { get; set; }
+}
